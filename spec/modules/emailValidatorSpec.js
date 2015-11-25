3b4f784a7a3411590f40538f463d2a4fafe39e71
Rafael Camargo <contato@rafaelcamargo.com>
2015-11-25 14:51:45 -0200

    Minor fixes

--- spec/modules/emailValidatorSpec.js
+++ spec/modules/emailValidatorSpec.js
@@ -3,8 +3,8 @@ define(['modules/emailValidator'],function(emailValidator) {
   describe('Email Validator', function(){
 
     it('should be true when email address is valid', function(){
-      var invalidEmail = 'test@valid.com';
-      expect(emailValidator.isValid(invalidEmail)).toEqual(true);
+      var validEmail = 'test@valid.com';
+      expect(emailValidator.isValid(validEmail)).toEqual(true);
     })
 
     it('should be false when email address is invalid', function(){
